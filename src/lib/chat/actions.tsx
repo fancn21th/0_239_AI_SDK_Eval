64df101f20a66149882ba594f16f2ebc1d9cb2ee
fancn21th <fancn21th@gmail.com>
2024-09-06 15:21:15 +0800

    📦 NEW: AI and UI States

--- src/lib/chat/actions.tsx
+++ src/lib/chat/actions.tsx
@@ -1,24 +1,81 @@
 "use server";
 
-import { streamText } from "ai";
+import { createAI, getMutableAIState, streamUI } from "ai/rsc";
 import { openai, MODEL_NAME } from "../provider/azure-openai";
-import { createStreamableValue } from "ai/rsc";
+import { ReactNode } from "react";
+// import { z } from "zod";
+import { generateId } from "ai";
 
-export async function generate(input: string) {
-  const stream = createStreamableValue("");
-
-  (async () => {
-    const { textStream } = await streamText({
-      model: openai(MODEL_NAME),
-      prompt: input,
-    });
-
-    for await (const delta of textStream) {
-      stream.update(delta);
-    }
-
-    stream.done();
-  })();
-
-  return { output: stream.value };
+export interface ServerMessage {
+  role: "user" | "assistant";
+  content: string;
 }
+
+export interface ClientMessage {
+  id: string;
+  role: "user" | "assistant";
+  display: ReactNode;
+}
+
+export async function continueConversation(
+  input: string
+): Promise<ClientMessage> {
+  "use server";
+
+  const history = getMutableAIState();
+
+  const messages = [...history.get(), { role: "user", content: input }];
+
+  const result = await streamUI({
+    model: openai(MODEL_NAME),
+    messages,
+    text: ({ content, done }) => {
+      if (done) {
+        history.done((messages: ServerMessage[]) => [
+          ...messages,
+          { role: "assistant", content },
+        ]);
+      }
+
+      return <div className="abc">{content}</div>;
+    },
+    // tools: {
+    //   showStockInformation: {
+    //     description:
+    //       "Get stock information for symbol for the last numOfMonths months",
+    //     parameters: z.object({
+    //       symbol: z
+    //         .string()
+    //         .describe("The stock symbol to get information for"),
+    //       numOfMonths: z
+    //         .number()
+    //         .describe("The number of months to get historical information for"),
+    //     }),
+    //     generate: async ({ symbol, numOfMonths }) => {
+    //       history.done((messages: ServerMessage[]) => [
+    //         ...messages,
+    //         {
+    //           role: "assistant",
+    //           content: `Showing stock information for ${symbol}`,
+    //         },
+    //       ]);
+    //       return <Stock symbol={symbol} numOfMonths={numOfMonths} />;
+    //     },
+    //   },
+    // },
+  });
+
+  return {
+    id: generateId(),
+    role: "assistant",
+    display: result.value,
+  };
+}
+
+export const AI = createAI<ServerMessage[], ClientMessage[]>({
+  actions: {
+    continueConversation,
+  },
+  initialAIState: [],
+  initialUIState: [],
+});
--- src/lib/provider/azure-openai.ts
+++ src/lib/provider/azure-openai.ts
@@ -4,7 +4,7 @@ export const MODEL_NAME = "GPT-4";
 
 export const openai = createAzure({
   resourceName: process.env.AZURE_RESOURCE_NAME, // Azure resource name
-  apiKey: process.env.AZURE_OPENAI_API_KEY!, // Azure API key
+  apiKey: process.env.AZURE_OPENAI_API_KEY, // Azure API key
   fetch: async (url, options) => {
     // const query = JSON.parse(options!.body! as string).messages[0].content
 
@@ -14,7 +14,7 @@ export const openai = createAzure({
       async start(controller) {
         const reader = originalResponse.body!.getReader();
 
-        console.log("Steam Started \n");
+        // console.log("Steam Started \n");
 
         while (true) {
           const { done, value } = await reader.read();
@@ -25,7 +25,7 @@ export const openai = createAzure({
             const encoder = new TextEncoder();
             const text = decoder.decode(value);
 
-            console.log("Text\n", text);
+            // console.log("Text\n", text);
 
             controller.enqueue(encoder.encode(text));
           } catch (error) {
